80975d1bf57da95c0e6e85c5cc7c2e582406f236
Manh Tran <tienmanh.tran68@gmail.com>
2022-03-12 14:21:58 +0700

    Creating a component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-function App() {
-  // const name = "Mike";
-  // const x = false;
+import Header from "./components/Header";
 
+function App() {
   return (
     <div className="container">
-      <h1>Hello From React</h1>
-      {/* <h2>Hello {name}</h2>
-      <h2>Hello {1 + 1}</h2>
-      <h2>Hello {x ? "yes" : "no"}</h2> */}
+      <Header />
     </div>
   );
 }
