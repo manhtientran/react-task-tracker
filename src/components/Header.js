7b5e285513c3389d30c0820d55c3d65f5ce85d9e
Manh Tran <tienmanh.tran68@gmail.com>
2022-03-12 14:51:00 +0700

    PropTypes and Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
+import PropTypes from "prop-types";
+
 const Header = ({ title }) => {
   return (
-    <header>
+    <header className="header">
+      {/* <h1 style={{ color: "red", backgroundColor: "black" }}>{title}</h1> */}
+      {/* <h1 style={headingStyle}>{title}</h1> */}
       <h1>{title}</h1>
+      <button className="btn">Add</button>
     </header>
   );
 };
@@ -9,5 +14,15 @@ const Header = ({ title }) => {
 Header.defaultProps = {
   title: "Task Tracker",
 };
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// CSS in JS
+// const headingStyle = {
+//   color: "red",
+//   backgroundColor: "black",
+// };
 
 export default Header;
