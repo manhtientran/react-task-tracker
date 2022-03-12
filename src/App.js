7134663564eb94db58a78705cfb8e4c97e5f1a43
Manh Tran <tienmanh.tran68@gmail.com>
2022-03-12 15:59:34 +0700

    Delete Task and Prop driliing

--- src/App.js
+++ src/App.js
@@ -24,10 +24,20 @@ function App() {
     },
   ]);
 
+  // Delete Task
+  const deleteTask = (id) => {
+    // console.log("delete", id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="container">
       <Header title="Hello" />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks to Show"
+      )}
     </div>
   );
 }
