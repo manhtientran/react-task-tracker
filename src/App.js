7a644fbfda77d6751e7606856d1511d7515d7efb
Manh Tran <tienmanh.tran68@gmail.com>
2022-03-12 17:54:58 +0700

    Fix some bugs and improve performance

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
@@ -90,22 +93,28 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <Header
-        title="Hello"
-        onAdd={() => {
-          setShowAddTask(!showAddTask);
-        }}
-        showAdd={showAddTask}
-      />
-      {showAddTask && <AddTask onAdd={addTask} />}
+    <BrowserRouter>
+      <div className="container">
+        <Header
+          onAdd={() => {
+            setShowAddTask(!showAddTask);
+          }}
+          showAdd={showAddTask}
+        />
+        {showAddTask && <AddTask onAdd={addTask} />}
 
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        "No Tasks to Show"
-      )}
-    </div>
+        {tasks.length > 0 ? (
+          <Tasks
+            tasks={tasks}
+            onDelete={deleteTask}
+            onToggle={toggleReminder}
+          />
+        ) : (
+          "No Tasks to Show"
+        )}
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; 2022</p>
+      <a href="/about">About Us</a>
+    </footer>
+  );
+};
+
+export default Footer;
