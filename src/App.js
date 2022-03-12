129fe5f383d1a7ea86928562450abe6d4791dc52
Manh Tran <tienmanh.tran68@gmail.com>
2022-03-12 14:14:30 +0700

    App component, JSX and its expressions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  // const name = "Mike";
+  // const x = false;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello From React</h1>
+      {/* <h2>Hello {name}</h2>
+      <h2>Hello {1 + 1}</h2>
+      <h2>Hello {x ? "yes" : "no"}</h2> */}
     </div>
   );
 }
