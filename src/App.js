4e901d60ea6f0aa70d33cee12db609b7aff00eea
Manh Tran <tienmanh.tran68@gmail.com>
2022-03-12 17:20:51 +0700

    Perform actions on tasks to the server

--- src/App.js
+++ src/App.js
@@ -25,31 +25,66 @@ function App() {
     return data;
   };
 
+  // Fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+
+    return data;
+  };
+
   // Add task
-  const addTask = (task) => {
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const newTask = await res.json();
+
     // console.log(task);
-    const id = Math.floor(Math.random() * 10000) + 1;
+    // const id = Math.floor(Math.random() * 10000) + 1;
     // console.log(id);
 
-    const newTask = {
-      id,
-      ...task,
-    };
+    // const newTask = {
+    //   id,
+    //   ...task,
+    // };
     setTasks([...tasks, newTask]);
   };
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    });
+
     // console.log("delete", id);
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
   // Toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+
+    const data = await res.json();
+
     // console.log(id);
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
